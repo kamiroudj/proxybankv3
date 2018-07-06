7f7e0a414bcfccee85329fb35bf86f8c8d9aa29d
kamiroudj = kamirelsisi2017@gmail.com <kamirelsisi2017@gmail>
2018-07-06 14:56:44 +0200

    ajout de la fonctionnalité virements

--- src/main/java/fr/gtm/proxibanque/business/OperationsBancairesService.java
+++ src/main/java/fr/gtm/proxibanque/business/OperationsBancairesService.java
@@ -26,26 +26,28 @@ public class OperationsBancairesService {
 	private CompteRepository daocp;
 
 
-	public void retraitLiquide(Integer compteDebit, Double montant) throws RetraitException {
+	public void retraitLiquide(Integer compteDebit, Double montant) throws CompteException {
 		
 		Optional<Compte> cp = daocp.findById(compteDebit);
 		Compte aDebiter = null;	
 		
 		if (cp.isPresent()) aDebiter = cp.get();
 		
-		if (aDebiter.getLibelle().equals(TypeCompte.COMPTE_EPARGNE.name())) throw new RetraitException("Vous ne pouvez pas retirer d'un compte epargne");
-		if (montant > 300 ) throw new RetraitException("Vous ne pouvez pas retirer plus de 300 euros");
+		if (aDebiter.getLibelle().equals(TypeCompte.COMPTE_EPARGNE.name())) throw new CompteException("Vous ne pouvez pas retirer d'un compte epargne");
+		if (montant > 300 ) throw new CompteException("Vous ne pouvez pas retirer plus de 300 euros");
 		if (montant > aDebiter.getSolde()) {
-			throw new RetraitException("Votre solde est insuffisant");
+			throw new CompteException("Votre solde est insuffisant");
 		}else {
 			aDebiter.setSolde(aDebiter.getSolde() - montant);
 			daocp.save(aDebiter);
-		}				
+		}
+		
+		
 	}
 
 
 	
-	public void retraitChequier(Integer idCompte) throws RetraitException {
+	public void retraitChequier(Integer idCompte) throws CompteException {
 		
 		Optional<Compte> cp = daocp.findById(idCompte);
 		Compte cpCheque = null;	
@@ -55,51 +57,49 @@ public class OperationsBancairesService {
 		
 		if (cpCheque.getChequier() != null) {
 			Period p = Period.between(cpCheque.getChequier().getDateReception(), today);
-			if ((p.getMonths()+p.getYears()*12) < 3 && p.getDays() != 0) throw new RetraitException("Vous ne pouvez pas retirer un chequier à moins de 3 mois");
+			if (p.toTotalMonths() < 3 && p.getDays() != 0) {
+				throw new CompteException("Vous ne pouvez pas retirer un chequier à moins de 3 mois");		
+			}
+		
 		}
 				
 	}
 	
 	
-	public void retraitCarte(Integer idCompte, String type) throws RetraitException {
+	public void retraitCarte(Integer idCompte, String type) throws CompteException {
 		
 		Optional<Compte> cp = daocp.findById(idCompte);
 		Compte cpCB = null;	
 		if (cp.isPresent()) cpCB = cp.get();
 		LocalDate today = LocalDate.now();
-		if (cpCB.getLibelle().equals(TypeCompte.COMPTE_EPARGNE.name())) throw new RetraitException("Vous ne pouvez pas retirer une carte pour un compte epargne");
+		if (cpCB.getLibelle().equals(TypeCompte.COMPTE_EPARGNE.name())) throw new CompteException("Vous ne pouvez pas retirer une carte pour un compte epargne");
 		if (cpCB.getCarteBleue() != null) {			
 			Period p = Period.between(cpCB.getCarteBleue().getDateExpiration(), today);			
-			if (p.toTotalMonths()*30+p.getDays() < 0) throw new RetraitException("Vous ne pouvez pas retirer une carte avant son expiration");
+			if(cpCB.getCarteBleue().getDateExpiration().isAfter(today)) throw new CompteException("Vous ne pouvez pas retirer une carte avant son expiration");
 		}
+		
 
 	}
 
 
-	public void virement(Integer idCompteDebit, Integer idCompteCredit, Double montant) throws SoldeException {
+	public void virement(Integer idCompteDebit, Integer idCompteCredit, Double montant) throws CompteException {
 		
 		Compte compteDebit = null;
 		Compte compteCredit = null;
-		//Compte compteDebit = daocp.findById(compDeb); //à modifier		
-		//Compte compteCredit = daocp.findById(compCre); //à modifier
 		
-		/*if (compteDebit instanceof CompteCourant) {
-			if (montant > (compteDebit.getSolde() + ((CompteCourant)compteDebit).getDecouvert())) throw new SoldeException("Solde insuffisant");
-			compteDebit.setSolde(compteDebit.getSolde() -  montant);
-			compteCredit.setSolde(compteCredit.getSolde() + montant);
-			daocp.save(compteDebit);
-			daocp.save(compteCredit);
-		}else {			
-			if (montant > compteDebit.getSolde()) throw new SoldeException("Solde insuffisant");
-			compteDebit.setSolde(compteDebit.getSolde() -  montant);
-			compteCredit.setSolde(compteCredit.getSolde() + montant);
-			daocp.save(compteDebit);
-			daocp.save(compteCredit);		
-		}*/
+		Optional<Compte> cd = daocp.findById(idCompteDebit);
+		Optional<Compte> cc = daocp.findById(idCompteCredit);
+		if (cd.isPresent()) compteDebit = cd.get();
+		if (cc.isPresent()) compteCredit = cc.get();
+		if (montant > 900 ) throw new CompteException("Vous ne pouvez pas retirer plus de 900 euros");	
+		if (montant > compteDebit.getSolde()) throw new CompteException("Solde insuffisant");
+		
+		compteDebit.setSolde(compteDebit.getSolde() -  montant);
+		compteCredit.setSolde(compteCredit.getSolde() + montant);
+		daocp.save(compteDebit);
+		daocp.save(compteCredit);		
 
 	}
-
-
 
 	public List<Compte> findComptes(Integer idClient) {
 
---
+++ src/main/webapp/WEB-INF/views/virement.jsp
@@ -0,0 +1,44 @@
+<%@ include file="head.jsp"%>
+        <div class="row flex-center full-screen my-4">
+            <div class="col-md-6">
+                <!-- Card -->
+                  <span>${erreur}</span>
+                <div class="card">
+
+
+                    <div class="card-body">
+                        <form method="POST">
+                       	 	<input type="hidden" name="idClient" value="${idClient}"/>
+                            <p class="h4 text-bank-light text-center py-4">Virement</p>
+
+                            <label for="compteDebit" class="font-weight-light">Choix du compte à débiter :</label>
+                            <select class="form-control" id="compteDebit" name="compteDebit">
+                           		<c:forEach var="compte" items="${comptes}">
+									<option value="${compte.id}">${compte.numCompte} -- ${compte.libelle} -- solde: ${compte.solde}</option>
+								 </c:forEach>
+                            </select>
+
+                            <label for="compteDebit" class="font-weight-light">Choix du compte à créditer :</label>
+                            <select class="form-control" id="compteCredit" name="compteCredit">
+                		<c:forEach var="compte" items="${comptes}">
+									<option value="${compte.id}">${compte.numCompte} -- ${compte.libelle} -- solde: ${compte.solde}</option>
+								 </c:forEach>
+                            </select>
+
+
+                            <div class="md-form">
+                                <i class="fa fa-euro prefix grey-text"></i>
+                                <input type="number" id="montant" class="form-control" name="montant" required>
+                                <label for="montant" class="font-weight-light">Montant maximum de retrait 900 euros</label>
+                            </div>
+
+                            <div class="text-center py-4 mt-3">
+                                <button class="btn btn-success" type="submit">Valider</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  <%@ include file="footer.jsp"%>
