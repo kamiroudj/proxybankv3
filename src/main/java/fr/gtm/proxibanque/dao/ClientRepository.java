0e95b92e7af67de2233f8a6ad9fcee5ef398595e
Arno <vallette.arnaud@gmail.com>
2018-07-06 14:46:53 +0200

    ajout de action-user.jsp menu fixe pour deconnection et retour au dashboard

--- src/main/java/fr/gtm/proxibanque/dao/ClientRepository.java
+++ src/main/java/fr/gtm/proxibanque/dao/ClientRepository.java
@@ -11,8 +11,19 @@ import fr.gtm.proxibanque.domain.Client;
 
 public interface ClientRepository extends JpaRepository<Client, Integer>{
 	
-	
+	/**
+	 * Recupère un client à partir de son Nom OU de son Prenom
+	 * @param prenom
+	 * @param nom
+	 * @return Client
+	 */
 	 public Client findByPrenomOrNom(String prenom, String nom);
 	 
+	 /**
+		 * Recupère un client à partir de son Nom ET de son Prenom
+		 * @param prenom
+		 * @param nom
+		 * @return Client
+		 */
 	 public Client findByPrenomAndNom(String prenom, String nom);
 }
---
+++ src/main/webapp/WEB-INF/views/action-user.jsp
@@ -0,0 +1,9 @@
+
+
+<div class="list-group actionorga">
+                <a href="#" class="list-group-item list-group-item-action active">
+                    ACTIONS
+                </a>
+                <a href="<c:url value='/dashboard.html'/>" class="list-group-item list-group-item-action">Retour à la liste des comptes</a>
+                <a href="<c:url value='/'/>" class="list-group-item list-group-item-action">Deconnecter</a>
+</div>
